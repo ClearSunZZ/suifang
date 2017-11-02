6621e28324962bf201a23422b1ae6eb7c927f5ce
Clear Sun <clear@chope.co>
2017-11-02 14:30:51 +0800

    Initial Commit
    Initial Commit

---
+++ suifang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  suifang
+//
+//  Created by 孙朋飞 on 2017/11/2.
+//  Copyright © 2017年 clear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ suifang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  suifang
+//
+//  Created by 孙朋飞 on 2017/11/2.
+//  Copyright © 2017年 clear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
